8d51f6c2eab21cdfaa12f7557042b138e3fccba8
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-26 22:26:40 -0800

    API working

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const dotenv = require("dotenv").config();
 const express = require("express")
 const cors = require("cors")
+
 const server = express()
 const port = process.env.PORT || 9001;
 
-console.log("Its alive!!")
+// console.log("Its alive!!")
+// console.log(process.env.PORT, process.env.NODE_ENV)
 
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/*", (req, res) => {
+
+server.get("/api/hello", (req, res) => {
     res.json({
-        message: "Server is running on ${port} YAY!!"
+        message: "api is working!"
     })
 })
+
+server.use("/api/*", (req, res) => {
+    res.send(`<h1>Hello world!</h1`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.messge,
+        stack: err.stack,
+    })
+})
+
+// ??? What is a stack???
 
 server.listen(port, () => {
     console.log(`Success!! Server is running on ${port}`)
